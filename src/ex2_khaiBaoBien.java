4934efd3ef71c25ca16203486a1c710311c46727
Ngo Gia Khanh <ngogiakhanh@MacBook-Pro-cua-Ngo.local>
2025-04-04 10:47:47 +0700

    ex2

---
+++ src/ex2_khaiBaoBien.java
@@ -0,0 +1,17 @@
+public class ex2_khaiBaoBien {
+    public static void main(String[] args) {
+        int i = 10;
+        System.out.println(i);
+        float f = 20.5f;
+        System.out.println(f);
+        double d = 30.5;
+        System.out.println(d);
+        boolean b = true;
+        System.out.println(b);
+        char c = 'a';
+        System.out.println(c);
+        String s = "Hello World!";
+        System.out.println(s);
+
+    }
+}
