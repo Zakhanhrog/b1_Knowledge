b64dd82049b77c14228f3a9a09339ffeb5b6d3dd
Ngo Gia Khanh <ngogiakhanh@MacBook-Pro-cua-Ngo.local>
2025-04-04 14:12:07 +0700

    ex6

---
+++ src/Ex6_.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Ex6_ {
+    public static void main(String[] args) {
+        Scanner inputYear = new Scanner(System.in);
+        System.out.println("Nhap nam muon kiem tra:  ");
+        int year = inputYear.nextInt();
+//        boolean result = false;
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ){
+            System.out.printf("Nam %d la nam nhuan",year );
+        }else {
+//            result = false;
+            System.out.printf("Nam %d khong phai la nam nhuan", year);
+        }
+
+//        if (result = true){
+//            System.out.printf("Nam %d la nam nhuan",year );
+//        }else{
+//            System.out.printf("Nam %d khong phai la nam nhuan", year);
+//        }
+    }
+}
